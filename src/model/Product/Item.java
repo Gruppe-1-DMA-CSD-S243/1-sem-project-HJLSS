bb77f53ac7bb650a484632a47715ea4a51708ce2
JonasHovaldt <10632099@ucn.dk>
2024-12-06 10:51:12 +0100

    Implemented classes Item and SimpleProduct
    Pair programming with Henrik, Sofus and Jonas

--- src/model/Product/Item.java
+++ src/model/Product/Item.java
@@ -2,4 +2,31 @@ package model.Product;
 
 public class Item {
 
+	private String serialNo;
+	private AdvancedProduct advancedProduct;
+	
+	public Item(String serialNo, AdvancedProduct advancedProduct) {
+		
+		this.serialNo = serialNo;
+		this.advancedProduct = advancedProduct;
+	}
+
+	public String getSerialNo() {
+		return serialNo;
+	}
+
+	public void setSerialNo(String serialNo) {
+		this.serialNo = serialNo;
+	}
+
+	public AdvancedProduct getAdvancedProduct() {
+		return advancedProduct;
+	}
+
+	public void setAdvancedProduct(AdvancedProduct advancedProduct) {
+		this.advancedProduct = advancedProduct;
+	}
+	
+	
+	
 }
--- src/model/Product/SimpleProduct.java
+++ src/model/Product/SimpleProduct.java
@@ -1,5 +1,19 @@
 package model.Product;
 
-public class SimpleProduct {
+public class SimpleProduct extends Product {
 
+	
+	public SimpleProduct(String barcode, String productNo, ProductCategory category, String name,
+			String manufacturerName, String manufacturerID, String model, String description, double price,
+			double costPrice, double msrp, int stockKeepingUnit) {
+		
+		super(barcode, productNo, category, name, manufacturerName, manufacturerID, model, description, price, costPrice, msrp,
+				stockKeepingUnit);
+		
+	}
+	
+	public String getBarcode() {
+		return super.getBarcode();
+	}
+	
 }
