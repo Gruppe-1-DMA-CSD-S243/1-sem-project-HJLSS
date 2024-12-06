634c88998c502f192679d0b0818acb9b8eeaa3e1
JonasHovaldt <10632099@ucn.dk>
2024-12-06 11:15:36 +0100

    Implemented class ProductController
    Pair programming with Henrik, Sofus and Jonas.

--- src/controller/ProductController.java
+++ src/controller/ProductController.java
@@ -1,5 +1,15 @@
 package controller;
 
+import model.Product.Product;
+import model.Product.ProductContainer;
+
 public class ProductController {
 
+	public ProductController() {
+		
+	}
+	
+	public Product findProductByBarcode(String barcode) {
+		return ProductContainer.getInstance().findProductByBarcode(barcode);
+	}
 }
