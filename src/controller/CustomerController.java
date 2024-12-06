0b105eed0fff78c84a2015007df1acbc8a3418d1
LasseKJ98 <10632102@ucn.dk>
2024-12-06 23:56:38 +0100

    Created CustomerController (attempt)
    Created the Class: CustomerController and added a FindByCVR method.

--- src/controller/CustomerController.java
+++ src/controller/CustomerController.java
@@ -1,5 +1,23 @@
 package controller;
 
-public class CustomerController {
+import java.util.HashMap;
 
+import model.Customer.Customer;
+
+public class CustomerController {
+private HashMap<String, Customer> businessCustomers;
+
+
+public CustomerController() {
+
+}
+public Customer findCustomerByCVR(String cvr) {
+	/*
+	 * for (Customer customer : customers) {
+	 * if(currentCustomer.getCVR().equalsIgnoreCase(cvr)) { return customer; } }
+	 * return null;
+	 */
+	Customer foundCustomer = businessCustomers.get(cvr);
+		return foundCustomer;
+}
 }
