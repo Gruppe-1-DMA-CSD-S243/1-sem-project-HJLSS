05c1c2d3890bbea6e48257a229bbc221acdd7073
SofusWennike <10647447@ucn.dk>
2024-12-09 11:46:52 +0100

    Syncing project

--- src/controller/MainTest.java
+++ src/controller/MainTest.java
@@ -2,6 +2,8 @@ package controller;
 import model.*;
 import model.Customer.BusinessCustomer;
 import model.Customer.CustomerContainer;
+import model.Order.Order;
+import model.Order.OrderContainer;
 import model.Product.Product;
 import model.Product.ProductCategory;
 import model.Product.ProductCategoryContainer;
@@ -14,7 +16,9 @@ public class MainTest {
 		// TODO Auto-generated method stub
 
 		SimpleProduct simpleProduct1 = new SimpleProduct("1", "1", null, "hammer", "Stanley", "123", "1000x", "stærk", 1000.00, 200.00, 500.00, 5);
+		SimpleProduct simpleProduct2 = new SimpleProduct("2", "1", null, "hammer", "Stanley", "123", "1000x", "stærk", 500.00, 200.00, 500.00, 5);
 		ProductContainer.getInstance().addProduct(simpleProduct1);
+		ProductContainer.getInstance().addProduct(simpleProduct2);
 		
 	    SimpleProduct foundProduct = (SimpleProduct)ProductContainer.getInstance().findProductByBarcode("1");
 		System.out.println(foundProduct.getBarcode());
@@ -22,7 +26,7 @@ public class MainTest {
 		BusinessCustomer businessCustomer1 = new BusinessCustomer("nameTest", "emailTest", "phoneTest", "addressTest", "cvrTest");
 		CustomerContainer.getInstance().addCustomer(businessCustomer1);
 		
-		BusinessCustomer foundCustomer = CustomerContainer.getInstance().findByCVR("cvrTest");
+		BusinessCustomer foundCustomer = CustomerContainer.getInstance().findCustomerByCVR("cvrTest");
 		System.out.println(foundCustomer.getCVR());
 		
 		ProductCategory productCategory1 = new ProductCategory("IDTest", "nameTest", "descriptionTest");
@@ -30,6 +34,27 @@ public class MainTest {
 		
 		ProductCategory foundProductCategory = ProductCategoryContainer.getInstance().findProductCategoryByProductCategoryID("IDTest");
 		System.out.println(foundProductCategory.getProductCategoryID());
+		
+		OrderController oc = new OrderController();
+		oc.makeNewOrder();
+		oc.addProductToOrder("1", 4);
+		oc.addProductToOrder("2", 1);
+		oc.addCustomerToOrder("cvrTest");
+		oc.makePayment(true);
+		oc.finishOrder();
+		
+		Order testOrder = OrderContainer.getInstance().getOrders().get("1");
+		
+		try {
+			System.out.println(testOrder.getOrderNo());
+			System.out.println(testOrder.getTimeOfPurchase());
+			System.out.println(testOrder.getCustomer().getCVR());
+			System.out.println(testOrder.getCustomer().getName());
+			System.out.println(testOrder.getPrice());
+		}
+		catch (Exception e) {
+			
+		}
 		
 	}
 	
--- src/model/Product/Product.java
+++ src/model/Product/Product.java
@@ -37,4 +37,8 @@ public abstract class Product {
 	public String getBarcode() {
 		return barcode;
 	}
+
+	public double getPrice() {
+		return price;
+	}
 }
