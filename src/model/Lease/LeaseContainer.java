b02b444bfd823d354505590f15b49ee4b11f0dce
JonasHovaldt <10632099@ucn.dk>
2025-01-14 09:36:50 +0100

    LeaseContainer
    implemented LeaseContainer

--- src/model/Lease/LeaseContainer.java
+++ src/model/Lease/LeaseContainer.java
@@ -1,6 +1,41 @@
 package model.Lease;
 
+import java.util.ArrayList;
+
 public class LeaseContainer {
 
-	int a = 1;
+	private static LeaseContainer instance;
+	private ArrayList<Lease> leases;
+	private Lease lease;
+	
+	private LeaseContainer() {
+		this.leases = new ArrayList<>();
+	}
+	
+	public static LeaseContainer getInstance() {
+		if(instance == null) {
+			instance = new LeaseContainer();
+		}
+		return instance;
+	}
+	
+	public Lease findLeaseByPhoneNumber(String phoneNumber) {
+		for(Lease lease: leases) {
+			if(lease.getCustomer().getPhone() == phoneNumber) {
+				return lease;
+			}
+		}
+	}
+	
+	public Lease findLeaseByToolID(String id) {
+		for(Lease lease: leases) {
+			if(lease.getTool().getID() == id) {
+				return lease;
+			}
+		}
+	}
+	
+	public void finishLease(Lease lease) {
+		leases.add(lease);
+	}
 }
