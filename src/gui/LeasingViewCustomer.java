c13e645c8847d2ca9b65ad095a7ba39d1e23c17f
JonasHovaldt <10632099@ucn.dk>
2025-01-15 11:53:22 +0100

    Update LeasingViewCustomer.java
    update to LeasingViewCustomer.

--- src/gui/LeasingViewCustomer.java
+++ src/gui/LeasingViewCustomer.java
@@ -7,6 +7,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.GridBagLayout;
 
 public class LeasingViewCustomer extends JDialog {
 
@@ -32,9 +33,14 @@ public class LeasingViewCustomer extends JDialog {
 	public LeasingViewCustomer() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[]{0};
+		gbl_contentPanel.rowHeights = new int[]{0};
+		gbl_contentPanel.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{Double.MIN_VALUE};
+		contentPanel.setLayout(gbl_contentPanel);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
